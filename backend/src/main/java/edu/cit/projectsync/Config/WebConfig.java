1b8b4543500abfa6cc2718fef31a778612873420
Van Harvey Coca <vanharvey.coca@cit.edu>
2025-04-27 11:27:38 +0800

    made dockerfile

--- backend/src/main/java/edu/cit/projectsync/Config/WebConfig.java
+++ backend/src/main/java/edu/cit/projectsync/Config/WebConfig.java
@@ -1,4 +1,18 @@
 package edu.cit.projectsync.Config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://www.project-sync.xyz")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
 }
